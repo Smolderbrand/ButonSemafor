3532c0f5252d5c42e87406d69548089cd2b400c6
Smolderbrand <59597833+Smolderbrand@users.noreply.github.com>
2024-05-14 15:02:34 +0300

    Second review

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 var semaphor;
+var TEN_SECONDS = 10000;
 
 function start() {
-	document.getElementById("button").style.background = "#FF0000";
-	window.semaphor = 0;
-	var intervalId = setInterval(function() { clickButton() }, 10000);
+	document.getElementById("button").style.background = "red";
+	semaphor = 0;
+	var intervalId = setInterval(function() { changeColor() }, TEN_SECONDS);
 }
 
-function clickButton() {
-	if (window.semaphor == 0) {
-		document.getElementById("button").style.background = "#FFFF40";
+function changeColor() {
+	if (semaphor === 0) {
+		document.getElementById("button").style.background = "yellow";
 		window.semaphor = 1;
-	} else if (window.semaphor == 1) {
-		document.getElementById("button").style.background = "#00C000";
+	} else if (semaphor === 1) {
+		document.getElementById("button").style.background = "green";
 		window.semaphor = 2;
-	} else if (window.semaphor == 2) {
-		document.getElementById("button").style.background = "#FF0000";
+	} else if (semaphor === 2) {
+		document.getElementById("button").style.background = "red";
 		window.semaphor = 0;
 	}
 }
