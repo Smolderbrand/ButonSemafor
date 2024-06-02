52c34e1f54f8295d0129d06bd1c7a6479a6665d3
Smolderbrand <59597833+Smolderbrand@users.noreply.github.com>
2024-06-02 11:05:26 +0300

    Code simplification

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-var semaphor;
-var TEN_SECONDS = 10000;
+let semaphor;
+let TEN_SECONDS = 10000;
 
 function start() {
 	document.getElementById("button").style.background = "red";
 	semaphor = 0;
-	var intervalId = setInterval(function() { changeColor() }, TEN_SECONDS);
+	let intervalId = setInterval(function() { changeColor() }, TEN_SECONDS);
 }
 
 function changeColor() {
-	if (semaphor === 0) {
-		document.getElementById("button").style.background = "yellow";
-		window.semaphor = 1;
-	} else if (semaphor === 1) {
-		document.getElementById("button").style.background = "green";
-		window.semaphor = 2;
-	} else if (semaphor === 2) {
-		document.getElementById("button").style.background = "red";
-		window.semaphor = 0;
-	}
+	let defaultColors = ["yellow", "green", "red"];
+	document.getElementById("button").style.background = defaultColors[semaphor];
+	semaphor = (semaphor + 1) % 3;
 }
